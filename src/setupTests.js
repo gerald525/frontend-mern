8dd39dc3d1612ee9518ce94765051de1f36a9a5f
gerald525 <gerald.lloyd.us@gmail.com>
2019-07-16 16:07:22 +1000

    first tests added to login component

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,9 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import Enzyme, { shallow, render, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-configure({ adapter: new Adapter() });
+// // Set the default serializer for Jest to be the from enzyme-to-json
+// // This produces an easier to read (for humans) serialized format.
+// expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
+
+Enzyme.configure({ adapter: new Adapter() });
