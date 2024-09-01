3052c2f5c371703a9a8ce789645973af6ef7a6f0
gerald525 <gerald.lloyd.us@gmail.com>
2024-09-01 15:17:50 +0300

    eeee

--- change.sh
+++
@@ -1,8 +0,0 @@
-git filter-branch --env-filter '
-CORRECT_NAME="gerald525"
-CORRECT_EMAIL="gerald.lloyd.us@gmail.com"
-export GIT_COMMITTER_NAME="$CORRECT_NAME"
-export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-export GIT_AUTHOR_NAME="$CORRECT_NAME"
-export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-' --tag-name-filter cat -- --branches --tags
